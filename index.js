5b803797dd187a3c0b52dd490428f9901c326df6
leewellmacharia10@gmail.com <leewellmacharia10@gmail.com>
2022-08-20 04:06:58 +0300

    COmpletd lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2
+const num2 = 31
+const multiply(num1,num2)
+// Write your code 
+const multiply = (num1*num2);
+// expect(Number.isInteger(random)).to.be.true
+var random = Math.random()*1
 
-// Write your code here
+const num3 = 68
+const num4 = 8
+
+const mod = num3 % num4;
+
+const max = Mat.max(2,7,11,20);
